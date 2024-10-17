a728a5c6c30af84b87371f228a209a5c7ed97556
Septka <septkacode@gmail.com>
2024-10-17 14:01:17 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -11,6 +11,9 @@ kaplay({
 
 loadSprite("player", "assets/bean.png");
 
+//Keeps track of how many platforms have been made after starting.
+let platforms = 0;
+
 let score = 0;
 let speed = 300;
 setGravity(1400)
@@ -18,11 +21,12 @@ setGravity(1400)
 const player = add([
     sprite("player"),
     anchor("bot"),
-    pos(200,830),
+    pos(206,830),
     area(),
     body(),
 ]);
 
+//Ground
 add([
     rect(width(), 48),
     outline(4),
@@ -31,12 +35,14 @@ add([
     body({ isStatic: true }),
 ]);
 
-const platform = add([
-    rect(100, 50),
+//Starting platform
+add([
+    rect(100, 25),
     outline(4),
     anchor("top"),
     pos(206, 750),
     body({ isStatic: true }),
+    "platform",
 ]);
 
 
@@ -48,12 +54,12 @@ onKeyPress("space", () => {
 
 onKeyDown("a", () => {
     player.move(-speed, 0);
-})
+});
 
 
 onKeyDown("d", () => {
     player.move(speed, 0)
-})
+});
 
 const scoreLabel = add([
     text(score),
@@ -67,11 +73,28 @@ const scoreLabel = add([
 loop(0.5, () => {
     score++;
     scoreLabel.text = score;
-})
+});
 
-
+//Gives collision to the highest platform if the player goes above it.
 onUpdate(()=>{
-    if (player.pos.y < platform.pos.y) {
-    platform.use(area());
-    }
+        if (player.pos.y < get("platform")[platforms].pos.y) {
+            get("platform")[platforms].use(area());
+            }
+});
+
+//Upon landing on a platform, creates a new, higher platform.
+//This *does* make new platforms even when landing on lower platforms,
+//but that can be fixed later. I'm tired.
+player.onCollide("platform", () => {
+    const x = rand(50, width() - 50);
+    const y = get("platform")[platforms].pos.y - 130;
+    platforms++
+    add([
+        rect(100, 25),
+        outline(4),
+        anchor("top"),
+        pos(x, y),
+        body({ isStatic: true }),
+        "platform",
+    ]);
 });
